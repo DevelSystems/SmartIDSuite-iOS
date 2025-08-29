987536b3435d2ca1af8d87358e069157bb653c25
Alejandro <68039905+jarcasta@users.noreply.github.com>
2025-08-29 01:22:14 -0600

    build(xcframework): generate xcframework

---
+++ SmartIDSuite.xcframework/ios-arm64_x86_64-simulator/SmartIDSuite.framework/Headers/SmartIDSuite.h
@@ -0,0 +1,18 @@
+//
+//  SmartIDSuite.h
+//  SmartIDSuite
+//
+//  Created by Alejandro Ramirez on 12/08/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SmartIDSuite.
+FOUNDATION_EXPORT double SmartIDSuiteVersionNumber;
+
+//! Project version string for SmartIDSuite.
+FOUNDATION_EXPORT const unsigned char SmartIDSuiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartIDSuite/PublicHeader.h>
+
+
